0775106a62d0e38a71cba53bd0934ead83c711b9
castromaciel <castromaciel00@gmail.com>
2023-01-21 17:35:37 -0300

    feat: Add navigation in issues by number

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/jsx-one-expression-per-line */
 import { FC } from 'react'
 import { FiCheckCircle, FiInfo, FiMessageSquare } from 'react-icons/fi'
+import { useNavigate } from 'react-router-dom'
 import { Issue, State } from '../interfaces'
 
 interface Props {
@@ -9,29 +12,36 @@ interface Props {
 
 export const IssueItem: FC<Props> = ({
   issue
-}) => (
-  <div className="card mb-2 issue">
-    <div className="card-body d-flex align-items-center">
-      {
+}) => {
+  const navigate = useNavigate()
+
+  return (
+    <div
+      className="card mb-2 issue"
+      onClick={() => navigate(`/issue/issue/${issue.number}`)}
+    >
+      <div className="card-body d-flex align-items-center">
+        {
           issue.state === State.Open
             ? <FiInfo size={30} color="red" />
             : <FiCheckCircle size={30} color="green" /> 
         }     
 
-      <div className="d-flex flex-column flex-fill px-2">
-        <span>{issue.title}</span>
-        <span className="issue-subinfo">
-          #{issue.number} opened 2 days ago by
-          <span className="fw-bold">{issue.user.login}</span>
-        </span>
-      </div>
+        <div className="d-flex flex-column flex-fill px-2">
+          <span>{issue.title}</span>
+          <span className="issue-subinfo">
+            #{issue.number} opened 2 days ago by
+            <span className="fw-bold">{issue.user.login}</span>
+          </span>
+        </div>
 
-      <div className="d-flex align-items-center">
-        <img src={issue.user.avatar_url} alt="User Avatar" className="avatar" />
-        <span className="px-2">{issue.comments}</span>
-        <FiMessageSquare />
-      </div>
+        <div className="d-flex align-items-center">
+          <img src={issue.user.avatar_url} alt="User Avatar" className="avatar" />
+          <span className="px-2">{issue.comments}</span>
+          <FiMessageSquare />
+        </div>
 
+      </div>
     </div>
-  </div>
-)
+  )
+}
