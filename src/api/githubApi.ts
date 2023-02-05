f214181466d5237b0bb8e0c6ebc2d7c7e3149794
castromaciel <castromaciel00@gmail.com>
2023-02-05 13:25:07 -0300

    feat: Add deployment

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
-    Authorization: 'Bearer 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' 
+    Authorization: `${import.meta.env.VITE_GIT_AUTHORIZATION}`
   }
 })
